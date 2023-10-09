436ed80e04c112317db776f09fcab9fce26b85e3
Pedro Dias <phpdias@outlook.com>
2023-10-09 11:58:50 -0300

    chore [spring-initializr]: definindo configurações do projeto e dependências com o spring initializr.

---
+++ src/test/java/todolist/phpdias/com/github/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package todolist.phpdias.com.github.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
