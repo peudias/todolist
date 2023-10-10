da3299c57b4575027dd56b9c4a9e4b91b850ed3f
Pedro Dias <phpdias@outlook.com>
2023-10-10 18:58:52 -0300

    feat [validate-user]: validating user to not repeat username in database

--- src/main/java/todolist/phpdias/com/github/todolist/user/InterfaceUserRepository.java
+++ src/main/java/todolist/phpdias/com/github/todolist/user/InterfaceUserRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterfaceUserRepository extends JpaRepository<UserModel, UUID> {
-    
+    UserModel findByUsername(String username);
 }
