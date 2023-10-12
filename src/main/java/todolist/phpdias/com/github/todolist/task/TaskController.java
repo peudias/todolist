150a7cec39f68a5f2a3990dd6730fbd111dbd524
Pedro Dias <phpdias@outlook.com>
2023-10-12 13:11:06 -0300

    feat [date-time]: validating date time;

--- src/main/java/todolist/phpdias/com/github/todolist/task/TaskController.java
+++ src/main/java/todolist/phpdias/com/github/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package todolist.phpdias.com.github.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,22 @@ public class TaskController {
     private InterfaceTaskRepository taskRepository;
     
     @PostMapping("/")
-    public TaskModel createTask(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity createTask(@RequestBody TaskModel taskModel, HttpServletRequest request){
         var userId = request.getAttribute("userId");
         taskModel.setUserId((UUID) userId);
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getDateInit())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Init date needs be bigger than current date.");
+        } else if (currentDate.isAfter(taskModel.getDateEnd())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("End date needs be bigger than current date.");            
+        }
+
+        if(taskModel.getDateInit().isAfter(taskModel.getDateEnd())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Init date needs be less than end date.");
+        }
+
         var taskCreated = this.taskRepository.save(taskModel);
-        return taskCreated;
+        return ResponseEntity.status(HttpStatus.OK).body(taskCreated);
     }
 }
