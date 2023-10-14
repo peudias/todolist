f04bb11e28373db80954cd8df7736b0856d0390a
Pedro Dias <phpdias@outlook.com>
2023-10-14 12:36:52 -0300

    feat [exception]: title only can receive 50 caracters

--- src/main/java/todolist/phpdias/com/github/todolist/task/TaskModel.java
+++ src/main/java/todolist/phpdias/com/github/todolist/task/TaskModel.java
@@ -30,4 +30,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("Title only can receive 50 caracters.");
+        }
+        this.title = title;
+    }
 }
